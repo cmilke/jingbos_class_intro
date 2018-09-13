78f5e85dc73ec8757e1a7001c292426ebecd06fd
Christopher Milke <cmilke@mail.smu.edu>
2018-09-13 10:24:54 -0500

    tutorial02 finished

--- tutorial02_rootCosmetics/tree_reader.cxx
+++ tutorial02_rootCosmetics/tree_reader.cxx
@@ -24,7 +24,61 @@ void tree_reader() {
         heat_map->Fill(x_data,y_data);
     }
 
+    
+    //Create a canvas on which to draw our histogram.
+    //Note that if we did not create a canvas here,
+    //then one would be automatically created with line below
+    //"heat_map->Draw("colz");".
+    //We explicitly construct the canvas here so that we can
+    //refer to it later.
+    double width = 1400;
+    double height = 1000;
+    TCanvas* canvas = new TCanvas("c1", "canvas", width, height);
+
     //Draw 2D histogram with colored bins (col)
     //and a scale for the color (z) axis
     heat_map->Draw("colz");
+
+
+    /*Customize our heat map plot so it looks pretty*/
+
+    //Create x and y labels, then center them
+    heat_map->SetXTitle("Some Meaningless Data");
+    heat_map->SetYTitle("More Meaningless Data");
+    heat_map->GetXaxis()->CenterTitle();
+    heat_map->GetYaxis()->CenterTitle();
+
+    //Change the range of the axes.
+    //Yes we could have just done this at the top...
+    double x_axis_min = 0.0;
+    double x_axis_max = 5.0;
+    double y_axis_min = 0.0;
+    double y_axis_max = 9.0;
+    heat_map->GetXaxis()->SetRangeUser(x_axis_min,x_axis_max);
+    heat_map->GetYaxis()->SetRangeUser(y_axis_min,y_axis_max);
+
+    //Change color axis to logarithmic scale
+    canvas->SetLogz(true);
+
+    //Disable the annoying statistics box.
+    //You might be wondering where this "gStyle"
+    //variable came from. It is an object of type
+    //"TStyle", and was automatically created by ROOT
+    gStyle->SetOptStat(false);
+
+    //Set palette for color-axis.
+    //This "kBlueRedYellow" variable is an
+    //enum, defined as part of the "TColor" class
+    gStyle->SetPalette(kBlueRedYellow);
+
+
+    //Update the canvas, then
+    //save heat map to png and pdf.
+    //Note that Update is usually run automatically
+    //anytime you change something on the canvas.
+    //I'm only running it manually because I'm paranoid.
+    canvas->Update();
+    canvas->SaveAs("heat_map.png");
+    canvas->SaveAs("heat_map.pdf");
+    
 }
